b1afd5c663c382aec9104061a56826a7cbb3eaec
David Bukowski <davebdb@users.noreply.github.com>
2018-11-28 22:21:39 -0600

    Initial commit First time creating a git repository

---
+++ helloWorld.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# #Day1 of the #100DaysofCode
+# This is my first attempt at writing python code and using GitHub as well
+# information on main() and __name__ == "__main__" at the following URI
+# https://www.guru99.com/learn-python-main-function-with-examples-understand-main.html
+
+def main():
+	print("Hello World")
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
+
+
